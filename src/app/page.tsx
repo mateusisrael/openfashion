504887ee045f0b818aeee2c42ef124a24d84257d
mateusisrael <mateuss.i.fernandes@gmail.com>
2025-09-06 23:26:01 -0300

    OF-3-chore: structures routes and components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/ui/components/Button";
 import Icon from "@/ui/components/icons";
 
 
@@ -7,6 +8,7 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-amber-50">
       <Icon name="backward" className="w[18px] "/>
       <p className="text-black">Luxury Fashion & accessorys</p>
+      <Button></Button>
     </div>
   );
 }
