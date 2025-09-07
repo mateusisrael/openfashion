bd7624e58d7759bde17a7e00d0da1d4906e2a3e8
mateusisrael <mateuss.i.fernandes@gmail.com>
2025-09-06 22:31:48 -0300

    OF-2-chore: configure icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Tenor_Sans } from "next/font/google";
+import { Tenor_Sans } from "next/font/google";
 import "./globals.css";
 
 const tenorSans = Tenor_Sans({
